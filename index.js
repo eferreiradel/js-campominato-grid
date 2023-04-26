5cd91c4917518e0ed998fa98f9e73a85366e02a7
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-04-26 22:01:16 +0200

    feat[JS]: aggiunto bottone che genera la griglia

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-/* To dos's
-
-create un layout in html con un bottone
-creare una classe estetica per le caselle
-
-
-Il bottone una volta premuto carica una funzine:
-    la funzione appende a un container vuoto le caselle
-
-
-*/
-
 console.log("run -- OK");
 
 //bottoni
 let btnGridGenerator = document.querySelector("#btnGridGenerator");
 let gridContainer = document.querySelector("#gridContainer");
-generateGridTiem(25);
 
-function generateGridTiem(cells) {
+let celle = 100 - 1;
+btnGridGenerator.addEventListener("click", generateGrid);
+
+function generateGrid() {
   //craezione della cella
 
-  for (counter = 0; counter <= cells; counter++) {
+  for (counter = 0; counter <= celle; counter++) {
     let gridItem = document.createElement("button");
     gridItem.classList.add("card", "btn");
     gridItem.innerHTML = generateRandomInt();
